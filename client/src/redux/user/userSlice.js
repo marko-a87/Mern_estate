d0c5dcea1c9cca6f71fc1b347bb9388122f6b43c
marko-a87 <malco0253@gmail.com>
2024-07-20 19:38:13 -0500

    Added signout functionality

--- client/src/redux/user/userSlice.js
+++ client/src/redux/user/userSlice.js
@@ -46,6 +46,20 @@ const userSlice = createSlice({
     deleteUserFailure: (state, action) => {
       state.error = action.payload;
       state.loading = false;
+    },
+
+    signOutUserStart: (state) => {
+      state.loading = true;
+    },
+    signOutUserSuccess: (state, action) => {
+      state.currentUser = null;
+      state.loading = null;
+      state.error = null;
+    },
+
+    signOutUserFailure: (state, action) => {
+      state.error = action.payload;
+      state.loading = false;
     }
   }
 });
@@ -58,6 +72,9 @@ export const {
   updateUserSuccess,
   deleteUserStart,
   deleteUserFailure,
-  deleteUserSuccess
+  deleteUserSuccess,
+  signOutUserStart,
+  signOutUserSuccess,
+  signOutUserFailure
 } = userSlice.actions;
 export default userSlice.reducer;
