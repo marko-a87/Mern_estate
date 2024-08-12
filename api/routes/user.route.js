1503f5b5dee85658191a65865450505194fedf18
marko-a87 <malco0253@gmail.com>
2024-08-12 11:01:21 -0500

    create get user listings api-route

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -2,7 +2,8 @@ import express from "express";
 import {
   test,
   updateUser,
-  deleteUser
+  deleteUser,
+  getUserListings
 } from "../controllers/user.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
@@ -10,5 +11,6 @@ const router = express.Router();
 router.get("/test", test);
 router.post("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
+router.get("/listings/:id", verifyToken, getUserListings);
 
 export default router;
